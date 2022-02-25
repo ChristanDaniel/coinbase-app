716c78fd39b3aa4979cb335c24da229600863a8e
ChristianDaniel <christian_daniel04@hotmail.com>
2022-02-24 22:53:33 -0300

    add feature HomeContainer

---
+++ src/features/HomeContainer.tsx
@@ -0,0 +1,13 @@
+import { HomeContent } from './styles'
+
+const HomeContainer = (): JSX.Element => {
+  return (
+    <>
+      <HomeContent>
+          <h1>Hello World</h1>
+      </HomeContent>
+    </>
+  )
+}
+
+export default HomeContainer

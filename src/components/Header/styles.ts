4ae4ec9d1e04f693fc422874797b8d04e2b7bda9
ChristianDaniel <christian_daniel04@hotmail.com>
2022-02-27 12:50:07 -0300

    pequenas alterações

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -8,22 +8,22 @@ const HeaderContainer = styled.header`
   align-items: center;
   justify-content: space-between;
 
-  border-bottom: 1px solid #8b8b8b;
+  border-bottom: 1px solid #282b2f;
 
   h1 {
-    margin-left: 20px;
+    margin-left: 25px;
   }
 
   div {
     display: flex;
     align-items: center;
-    gap: 10px;
-    margin-right: 60px;
+    gap: 30px;
+    margin-right: 80px;
 
     P {
       display: flex;
       flex-direction: column;
-      border: 1px solid #8b8b8b;
+      border: 1px solid #282b2f;
       padding: 4px 17px 4px 17px;
       border-radius: 35px;
       font-size: 16px;
@@ -31,7 +31,7 @@ const HeaderContainer = styled.header`
 
       span {
         font-size: 12px;
-        color: #fff;
+        color: #8b8b8b;
         margin-top: 3px;
       }
     }
@@ -42,7 +42,7 @@ const HeaderContainer = styled.header`
 
     #button-receive {
       background-color: black;
-      border: 1px solid #8b8b8b;
+      border: 1px solid #282b2f;
     }
   }
 `

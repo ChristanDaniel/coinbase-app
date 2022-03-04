2addac6245387feb06fa1fadf9420bbdae3387f8
ChristianDaniel <christian_daniel04@hotmail.com>
2022-03-03 22:53:07 -0300

    formatting numberWallet

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,13 +8,18 @@ interface HeaderProps {
 }
 
 const Header = ({ numberWallet }: HeaderProps): JSX.Element => {
+
+  const formatWalletAdress = (WalletAdress: string) => {
+    return String(WalletAdress).substr(0, 7) + '...' + String(WalletAdress).substr(31, 11)
+  }
+
   return (
     <>
       <HeaderContainer>
         <h1>Assets</h1>
         <div>
           <p>
-            Wallet Connected <span>{numberWallet}</span>
+            Wallet Connected <span>{formatWalletAdress(numberWallet)}</span>
           </p>
 
           <Button id="button-buy">Buy / Sell</Button>

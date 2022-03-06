d9fa7a379cd6a21d8ba2a0c05c2d4b122b8b21fe
ChristianDaniel <christian_daniel04@hotmail.com>
2022-03-06 15:29:20 -0300

    create table portfolio

--- src/components/Portfolio/index.tsx
+++ src/components/Portfolio/index.tsx
@@ -1,10 +1,30 @@
-import { PortfolioContainer } from "./styles"
+import { PortfolioContainer, PortfolioContent } from './styles'
 
 const Portfolio = (): JSX.Element => {
   return (
     <>
       <PortfolioContainer>
-        <h1>HELLO</h1>
+        <PortfolioContent>
+          <PortfolioTable>
+            <TableItem>
+              <Title>Your Assets</Title>
+            </TableItem>
+            <Divider />
+            <Table>
+              <TableItem>
+                <TableRow>
+                  <div style={{ flex: 3 }}>Name</div>
+                  <div style={{ flex: 2 }}>Balance</div>
+                  <div style={{ flex: 1 }}>Price</div>
+                  <div style={{ flex: 1 }}>Allocation</div>
+                  <div style={{ flex: 0 }}>
+                  </div>
+                </TableRow>
+              </TableItem>
+              <Divider />
+            </Table>
+          </PortfolioTable>
+        </PortfolioContent>
       </PortfolioContainer>
     </>
   )
--- src/components/Portfolio/styles.ts
+++ src/components/Portfolio/styles.ts
@@ -1,5 +1,18 @@
 import styled from 'styled-components'
 
-const PortfolioContainer = styled.div``
+const PortfolioContainer = styled.div`
+  display: flex;
+  flex: 1;
+  justify-content: center;
+`
 
-export { PortfolioContainer }
+const PortfolioContent = styled.div`
+  width: 100%;
+  max-width: 1000px;
+  padding: 2rem 1rem;
+`;
+
+
+
+
+export { PortfolioContainer, PortfolioContent}
